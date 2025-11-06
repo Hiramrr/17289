c986c622cdf8f6daa3797739f4ec36cfd2706cb5
hiramrr <sasuke_77.23@hotmail.com>
2025-11-06 13:05:56 -0600

    Svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+    import Hijo from "./hijo.svelte";
+
+    //export let name;
+
+    let nombre = "";
+    let contador = 0;
+
+    function incrementar() {
+        contador += 1;
+    }
+
+    function saludar() {
+        alert(nombre ? nombre : "valor por omision");
+    }
+</script>
+
+<main>
+    <h1>Contar: {contador}</h1>
+    <button on:click={incrementar}>Contar ++</button>
+    <Hijo texto="Guardar" color="green"></Hijo>
+    <Hijo texto="Borrar" color="red"></Hijo>
+    <input type="text" bind:value={nombre} placeholder="Introduce tu nombre" />
+    <button on:click={saludar}>Saludar</button>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ svelte-app/src/hijo.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let texto = "hola miyu";
+    export let color = "blue";
+</script>
+
+<button style="background-color: {color};">
+    {texto}
+</button>
+
+<style>
+    button {
+        color: white;
+    }
+</style>
